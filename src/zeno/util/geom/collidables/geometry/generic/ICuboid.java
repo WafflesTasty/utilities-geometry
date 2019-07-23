6ded89cd339be7df93f94697fc28c3f53da3dd84
X-Zeno-X <zenothedefiant@gmail.com>
2019-07-23 17:35:58 +0200

    Initial commit.

--- src/zeno/util/geom/collidables/geometry/generic/ICuboid.java
+++ src/zeno/util/geom/collidables/geometry/generic/ICuboid.java
@@ -1,9 +1,8 @@
 package zeno.util.geom.collidables.geometry.generic;
 
-import zeno.util.algebra.linear.vector.Vector;
-import zeno.util.geom._deprecated.Containment;
-import zeno.util.geom._deprecated.Intersection;
+import zeno.util.geom.collidables.ICollision;
 import zeno.util.geom.collidables.IGeometry;
+import zeno.util.geom.collidables.collisions.geometry.CLSCuboid;
 
 /**
  * The {ICuboid} interface defines the collision operations for cuboid geometry.
@@ -18,48 +17,10 @@ import zeno.util.geom.collidables.IGeometry;
 public interface ICuboid extends IGeometry
 {	
 	@Override
-	public default boolean contains(Vector p)
+	public default ICollision Collisions()
 	{
-		return Containment.in(this, p);
+		return new CLSCuboid(this);
 	}
-		
-	@Override
-	public default boolean contains(IEllipsoid e)
-	{
-		return Containment.in(this, e);
-	}
-	
-	@Override
-	public default boolean contains(ICuboid c)
-	{
-		return Containment.in(this, c);
-	}
-	
-
-	@Override
-	public default boolean intersects(ICuboid c)
-	{
-		return Intersection.between(this, c);
-	}
-	
-	@Override
-	public default boolean intersects(IEllipsoid e)
-	{
-		return Intersection.between(this, e);
-	}
-		
-	@Override
-	public default boolean intersects(ISegment l)
-	{
-		return Intersection.between(this, l);
-	}
-	
-	@Override
-	public default boolean intersects(ISphere s)
-	{
-		return Intersection.between(this, s);
-	}
-
 	
 	// Optional Bounds overrides.
 	
