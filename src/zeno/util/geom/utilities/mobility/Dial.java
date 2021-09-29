2aad0c331dc65bc840cbe77895277cdabde98917
X-Zeno-X <zenothedefiant@gmail.com>
2021-09-29 15:42:31 +0200

    Added helper method for 2D cases.

--- src/zeno/util/geom/utilities/mobility/Dial.java
+++ src/zeno/util/geom/utilities/mobility/Dial.java
@@ -1,5 +1,9 @@
 package zeno.util.geom.utilities.mobility;
 
+import zeno.util.algebra.algorithms.solvers.SLVCrout;
+import zeno.util.algebra.linear.matrix.fixed.Matrix3x3;
+import zeno.util.algebra.linear.vector.fixed.Vector2;
+
 /**
  * The {@code Dial} enum defines clockwise and counter-clockwise motion.
  *
@@ -17,4 +21,38 @@ public enum Dial
 	 * Clockwise.
 	 */
 	CLOCKWISE;
+	
+	
+	/**
+	 * Computes the dial of a triangle.
+	 * 
+	 * @param a  a first point
+	 * @param b  a second point
+	 * @param c  a third point
+	 * @return  a three point dial
+	 * 
+	 * 
+	 * @see Vector2
+	 */
+	public static Dial of(Vector2 a, Vector2 b, Vector2 c)
+	{
+		Matrix3x3 mat = new Matrix3x3();
+		
+		mat.set(a.X(), 0, 0);
+		mat.set(b.X(), 0, 1);
+		mat.set(c.X(), 0, 2);
+		
+		mat.set(a.Y(), 1, 0);
+		mat.set(b.Y(), 1, 1);
+		mat.set(c.Y(), 1, 2);
+		
+		mat.set(1f, 2, 0);
+		mat.set(1f, 2, 1);
+		mat.set(1f, 2, 2);
+		
+		SLVCrout slv = new SLVCrout(mat);
+		if(slv.determinant() > 0)
+			return CNT_CLOCKWISE;
+		return CLOCKWISE;
+	}
 }
