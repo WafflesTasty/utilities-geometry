eea316605070ccf8adb8e23816bd8522b9b853a8
WafflesTasty <zenothedefiant@gmail.com>
2024-05-12 14:39:45 +0200

    Added spin and flip to Cardinal2D, update 1.1.

--- src/waffles/utils/geom/utilities/constants/Cardinal2D.java
+++ src/waffles/utils/geom/utilities/constants/Cardinal2D.java
@@ -1,6 +1,10 @@
 package waffles.utils.geom.utilities.constants;
 
 import waffles.utils.algebra.elements.linear.vector.fixed.Vector2;
+import waffles.utils.geom.collidable.fixed.Point;
+import waffles.utils.geom.spatial.data.spin.Spin2D;
+import waffles.utils.geom.spatial.maps.fixed.linear.Rotation;
+import waffles.utils.tools.primitives.Floats;
 import waffles.utils.tools.primitives.Integers;
 
 /**
@@ -53,21 +57,7 @@ public final class Cardinal2D extends Vector2
 	 */
 	public static Cardinal2D SOUTHEAST 	= new Cardinal2D( 1,-1);
 	
-	
-	/**
-	 * Flips a {@code Cardinal2D} in the opposite direction.
-	 * 
-	 * @param c  a cardinal direction
-	 * @return   a cardinal opposite
-	 */
-	public static Cardinal2D flip(Cardinal2D c)
-	{
-		Vector2 v = c.times(-1f);
-		int x = (int) v.X();
-		int y = (int) v.Y();
-		return create(x, y);
-	}
-
+		
 	/**
 	 * Creates a {@code Cardinal2D} in a given direction.
 	 * 
@@ -75,10 +65,10 @@ public final class Cardinal2D extends Vector2
 	 * @param y  an y-coordinate
 	 * @return  a cardinal
 	 */
-	public static Cardinal2D create(int x, int y)
+	public static Cardinal2D create(float x, float y)
 	{
-		int sx = Integers.sign(x);
-		int sy = Integers.sign(y);
+		int sx = (int) Floats.sign(x);
+		int sy = (int) Floats.sign(y);
 		
 		for(Cardinal2D c : All())
 		{
@@ -123,8 +113,52 @@ public final class Cardinal2D extends Vector2
 	{
 		return ALL;
 	}
+
+	
+	/**
+	 * Flips the {@code Cardinal2D} in the opposite direction.
+	 * 
+	 * @return  a cardinal opposite
+	 */
+	public Cardinal2D flip()
+	{
+		return create(-X(), -Y());
+	}
+
+	/**
+	 * Spins the {@code Cardinal2D} along a dial rotation.
+	 * 
+	 * @param d  a dial rotation
+	 * @return   a spun cardinal
+	 * 
+	 * 
+	 * @see Dial
+	 */
+	public Cardinal2D spin(Dial d)
+	{
+		Point p = new Point(this, 0f);
+		Rotation rot = new Rotation(Spin(d));
+		Point q = (Point) rot.map(p);
+		
+		float x = q.get(0);
+		float y = q.get(1);
+		
+		return create(x, y);
+	}
 	
 	
+	private static Spin2D Spin(Dial d)
+	{
+		switch(d)
+		{
+		case CNT_CLOCKWISE:
+			return new Spin2D(+Floats.PI / 4);
+		case CLOCKWISE:
+			return new Spin2D(-Floats.PI / 4);
+		default:
+			return null;
+		}
+	}
 	
 	private static Cardinal2D[] ALL = new Cardinal2D[]{CENTER, EAST, NORTHEAST, NORTH, NORTHWEST, WEST, SOUTHWEST, SOUTH, SOUTHEAST};
 	
